0691f1f7d75edce100341df0565316fdc4b38ca4
Pablo Bacchetta <polbac@gmail.com>
2018-09-03 18:21:13 -0300

    some logic

--- public/src/components/authentication-protection/index.js
+++ public/src/components/authentication-protection/index.js
@@ -2,33 +2,24 @@ import React, { Component } from 'react';
 
 export default class AuthenticationProtection extends Component {
     
-    componentDidlMount(){
-        const liLogin = function() { // Setup an event listener to make an API call once auth is complete
-            window.IN.UI.Authorize().params({"scope":["r_basicprofile", "r_emailaddress"]}).place();
-            window.IN.Event.on(window.IN, 'auth', getProfileData);
-        }
-    
-        var getProfileData = function() { // Use the API call wrapper to request the member's basic profile data
-        console.log(1)    
-        window.IN.API.Profile("me").fields("id,firstName,lastName,email-address,picture-urls::(original),public-profile-url,location:(name)").result(function (me) {
-                console.log(me);
-                var profile = me.values[0];
-                var id = profile.id;
-                var firstName = profile.firstName;
-                var lastName = profile.lastName;
-                var emailAddress = profile.emailAddress;
-                var pictureUrl = profile.pictureUrls.values[0];
-                var profileUrl = profile.publicProfileUrl;
-                var country = profile.location.name;
-            });
+    constructor(props) {
+        super(props);
+        this.state = {
+            authenticated: false,
         }
     }
 
-    render(){
-        return (
-            <div>
-                <script type="in/Login"></script>
-            </div>
-        );
+    componentDidlMount(){
+       
+    }
+
+    render() {
+        const { authenticated } = this.state;
+        
+        if (!authenticated) {
+            return <script type="in/Login"></script>;
+        }
+        
+        return this.props.children;
     }   
 }
