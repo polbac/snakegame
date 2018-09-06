8750fe039a8663c6b6bfbd681887753b6d32a649
Eliana De Luca <elianadeluca@hotmail.com>
2018-09-06 09:22:50 -0300

    Add Qr plugin

--- public/src/containers/hall-of-fame/index.tsx
+++ public/src/containers/hall-of-fame/index.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
+import * as QRCode from 'qrcode.react';
+import config from '../../config';
 
 export default class HallOfFame extends React.Component<{}> {
     render(){
-        return (<section>Hall Of Fame</section>);
+        return (
+            <section>
+                <QRCode value={config.appUrl} />
+            </section>
+        );
     }   
 }
