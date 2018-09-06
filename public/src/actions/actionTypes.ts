25798467529a192ecc09050b1eec5bc6086794ba
Pablo Bacchetta <polbac@gmail.com>
2018-09-06 11:48:09 -0300

    linkedin authenticate

---
+++ public/src/actions/actionTypes.ts
@@ -0,0 +1,21 @@
+
+export const SAVE_SESSION: string = 'SAVE_SESSION';
+
+export const GAME: any = {
+
+    START: 'START_GAME',
+    END: 'END_GAME',
+    RESTART: 'RESTART_GAME',
+
+    SNAKE_MOVE_TOP: 'SNAKE_MOVE_TOP',
+    SNAKE_MOVE_LEFT: 'SNAKE_MOVE_LEFT',
+    SNAKE_MOVE_BOTTOM: 'SNAKE_MOVE_BOTTOM',
+    SNAKE_MOVE_RIGHT: 'SNAKE_MOVE_RIGHT',
+
+    SNAKE_EAT: 'SNAKE_EAT',
+
+};
+
+export default {
+    SAVE_SESSION,
+};
