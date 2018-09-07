3cc07ef75eb078c15472829dec7c2c99d2a276bc
Eliana De Luca <elianadeluca@hotmail.com>
2018-09-07 19:03:05 -0300

    game over

--- client/src/components/layout/index.tsx
+++ client/src/components/layout/index.tsx
@@ -22,6 +22,12 @@ export const Content: any = styled.div`
         height:100vh;
         background-image: linear-gradient(to top, #ebf9f9, #ebf9f9);
     `}
+    ${(p: any) => p.over && css`
+        padding: 0 30px;
+        text-align:center;
+        height:100vh;
+        background-image: linear-gradient(to top, #ebf9f9, #ebf9f9);
+    `}
 
      ${(p: any) => p.game && css`
         padding: 10px 20px;
