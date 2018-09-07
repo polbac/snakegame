34ae504f04ccd2d67f8dedd921a4e8eb947aaa24
Eliana De Luca <elianadeluca@hotmail.com>
2018-09-07 19:06:54 -0300

    fix wrong commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,14 +6,13 @@ import { HallOfFame } from './containers/hall-of-fame';
 import './App.css';
 import { connect } from 'react-redux';
 import { showMainMenu } from './actions/game';
-import { endGame } from './actions/game';
 
 @connect(state => state)
 export default class App extends React.Component<{}> {
     
     componentDidMount() {
         const { dispatch } = this.props as any;
-        dispatch(endGame());
+        dispatch(showMainMenu());
     }
     render(){
         return (
