24cfbe4436d3e5a7fe4bede29f2e4abf37485646
Pablo Bacchetta <polbac@gmail.com>
2018-09-07 14:09:15 -0300

    change name

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 
 export default {
-    appURL: 'http://192.168.1.83:3000',
-    serverURL: 'http://192.168.1.83:4000'
+    appUrl: 'http://192.168.1.83:3000',
+    serverUrl: 'http://192.168.1.83:4000'
 };
