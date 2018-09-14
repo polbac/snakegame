87a3b476d749e2cda7dfb05e35f0ef919f7ba76e
Pablo Bacchetta <polbac@gmail.com>
2018-09-14 17:22:36 -0300

    fix assets

--- client/src/components/game-frame/index.tsx
+++ client/src/components/game-frame/index.tsx
@@ -149,6 +149,7 @@ export class GameFrame extends React.Component<GameFrameProps, GameFrameState> {
         this.pixiApp = new PIXI.Application(window.innerWidth, window.innerHeight, {
             view: document.getElementById(this.state.canvasId) as HTMLCanvasElement,
         });
+
         this.pixiApp.renderer.backgroundColor = 0xffffff;
         
         this.player = PIXI.Sprite.fromImage('assets/images/head.png');
